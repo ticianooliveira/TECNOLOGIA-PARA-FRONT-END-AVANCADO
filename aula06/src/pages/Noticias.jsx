c05bc43e22df9bca13948e9662dd1e6ea5c30997
Ticiano <ticianoodonto@gmail.com>
2024-10-30 20:32:10 -0300

    Uso do Axios
    Uso do Axios

--- aula06/src/pages/Noticias.jsx
+++ aula06/src/pages/Noticias.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom'
 const url = "http://localhost:3000/noticias"
 
 const Noticias = () => {
@@ -22,8 +23,7 @@ const Noticias = () => {
             {
                 noticias.map((noticia) => (
                     <li key={noticia.id}>
-                        <h2>{noticia.titulo}</h2>
-
+                        <h2><Link to={`/visualiza-noticia/${noticia.id}`}>{noticia.titulo}</Link></h2>
                     </li>
                 ))
             }
