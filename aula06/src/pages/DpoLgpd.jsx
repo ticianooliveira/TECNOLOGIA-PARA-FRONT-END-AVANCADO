98c83266e98f728d1ebed3a6dd3ec97ca0c20d13
Ticiano <ticianoodonto@gmail.com>
2024-10-30 19:37:15 -0300

    React Router
    Implementando o React Router

---
+++ aula06/src/pages/DpoLgpd.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DpoLgpd = () => {
+  return (
+    <div>
+        Aqui será a página dpo
+    </div>
+  )
+}
+
+export default DpoLgpd
---
+++ aula06/src/pages/Inicial.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Inicial = () => {
+  return (
+    <div>
+        Aqui será a página inicial</div>
+  )
+}
+
+export default Inicial
