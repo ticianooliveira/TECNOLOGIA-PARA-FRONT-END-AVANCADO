fc84c9af75345aa5e0f8266df221e37c8944f167
Ticiano <126998734+ticianooliveira@users.noreply.github.com>
2024-11-14 00:02:27 -0300

    Add files via upload

--- site_uniesp_2024_2/src/App.jsx
+++ site_uniesp_2024_2/src/App.jsx
@@ -4,6 +4,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import BannerAd from './components/BannerAd'
 import Inicial from './pages/Inicial'
+import Faculdade from './pages/Faculdade'
+import DpoLgpd from './pages/DpoLgpd'
+import Noticias from './pages/Noticias'
+import VisualizaNoticia from './pages/VisualizaNoticia'
+import AdminNoticias from './pages/admin/AdminNoticias'
+import CadastroNoticia from './pages/admin/CadastroNoticias'
+import EditarNoticia from './pages/admin/EditarNoticia'
 
 const App = () => {
   return (
@@ -20,6 +27,13 @@ const App = () => {
 
           <Routes>
             <Route path="/" element={<Inicial />} />
+            <Route path="/a-faculdade" element={<Faculdade />} />
+            <Route path="/dpo-lgpd" element={<DpoLgpd />} />
+            <Route path="/noticias" element={<Noticias />} />
+            <Route path="/visualiza-noticia/:id" element={<VisualizaNoticia />} />
+            <Route path="/admin-noticias" element={<AdminNoticias />} />
+            <Route path="/cadastrar-noticia" element={<CadastroNoticia />} />
+            <Route path="/editar-noticia/:id" element={<EditarNoticia />} />
           </Routes>
 
         </Box>
